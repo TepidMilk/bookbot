42b08bceb4779c0b8b957c896c42d90b19857da7
TepidMilk <ivorykingrey@gmail.com>
2024-09-18 15:58:16 -0500

    Add get_file_name and book_report function to main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
     text = get_text(path_to_file)
     num_words = word_count(text)
     characters = char_count(text)
-    
+    book_report(get_file_name(path_to_file), num_words, characters)
 
 def get_text(path):
     with open(path) as f:
@@ -23,5 +23,17 @@ def char_count(text):
             char_dict[char] += 1
     return char_dict
 
+def get_file_name(file_path):
+    file = file_path.split("/")
+    return file[1].strip(".txt")
+
+def book_report(book, words, chars):
+    chars_sort = dict(sorted(chars.items(), key=lambda x: x[1], reverse=True))
+    print(f"--- begin report of book: {book} ---")
+    print(f"{words} words found in the document \n")
+    for char in chars_sort:
+        if char.isalpha():
+            print(f"The '{char}' character was found {chars[char]} times")
+    print("--- End Report ---")
 
 main()
