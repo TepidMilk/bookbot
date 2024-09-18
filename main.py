a28d8eacd08952deda4b6ca4ff73cd72ed043304
TepidMilk <ivorykingrey@gmail.com>
2024-09-18 15:57:11 -0500

    add char_count function to main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     path_to_file = "books/frankenstein.txt"
     text = get_text(path_to_file)
     num_words = word_count(text)
+    characters = char_count(text)
     
 
 def get_text(path):
@@ -11,5 +12,16 @@ def get_text(path):
 def word_count(text):
     words = text.split()
     return len(words)
+
+def char_count(text):
+    lower_text = text.lower()
+    char_dict = {}
+    for char in lower_text:
+        if char not in char_dict:
+            char_dict[char] = 1
+        else:
+            char_dict[char] += 1
+    return char_dict
+
 
 main()
